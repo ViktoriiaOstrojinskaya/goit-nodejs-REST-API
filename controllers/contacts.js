3f071cb4bbfaaa98bd286fc5e85f3787e07a84bc
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-03-02 11:09:56 -0500

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,7 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 2 } = req.query;
   const skip = (page - 1) * limit;
   const data = await Contact.find({ owner: _id }, "", {
     skip,
--- controllers/users.js
+++ controllers/users.js
@@ -56,8 +56,8 @@ const logout = async (req, res) => {
 };
 
 const current = async (req, res) => {
-  const { email } = req.user;
-  res.status(200).json({ email });
+  const { email, subscription } = req.user;
+  res.status(200).json({ email, subscription });
 };
 
 module.exports = {
