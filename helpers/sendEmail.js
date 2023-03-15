bd7b001007adc313e3e124b668b6d901b66a9d46
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-03-15 11:51:01 -0400

    created Dockerfile

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -6,13 +6,9 @@ const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  try {
-    const email = { ...data, from: "viktoriia.ostrojinskaya@gmail.com" };
-    await sgMail.send(email);
-    return true;
-  } catch (error) {
-    console.error(error);
-  }
+  const email = { ...data, from: "viktoriia.ostrojinskaya@gmail.com" };
+  await sgMail.send(email);
+  return true;
 };
 
 module.exports = sendEmail;
